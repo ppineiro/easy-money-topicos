3389b528a8d66329ea87526c507ae1912e4e7b51
Pablo Piñeiro <pmpv05@hotmail.com>
2019-10-15 12:21:48 -0300

    ruteo

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,10 @@
 const users = require('./users');
+const divisas = require('./divisas');
+const voluntades = require('./voluntades');
+const propuestas = require('./propuestas');
+const transacciones = require('./transacciones');
 
-const resourceRoutes = [users];
+const resourceRoutes = [voluntades, propuestas, transacciones, divisas, users];
 
 module.exports = router => {
   resourceRoutes.forEach(routes => routes(router));
