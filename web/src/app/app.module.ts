7cb602ed9358790325f7c33262b07ceea44e379f
Pablo Piñeiro <pmpv05@hotmail.com>
2019-10-15 16:13:34 -0300

    agrego tema nebular

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { IngresoVoluntadComponent } from './components/ingreso-voluntad/ingreso-voluntad.component';
-import { VoluntadComponent } from './components/voluntad/voluntad.component';
-import { ListaVoluntadesComponent } from './components/lista-voluntades/lista-voluntades.component';
-import { PropuestaComponent } from './components/propuesta/propuesta.component';
-import { TransaccionComponent } from './components/transaccion/transaccion.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { LoadingComponent } from './components/shared/loading/loading.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegistroComponent } from "./components/registro/registro.component";
+import { IngresoVoluntadComponent } from "./components/ingreso-voluntad/ingreso-voluntad.component";
+import { VoluntadComponent } from "./components/voluntad/voluntad.component";
+import { ListaVoluntadesComponent } from "./components/lista-voluntades/lista-voluntades.component";
+import { PropuestaComponent } from "./components/propuesta/propuesta.component";
+import { TransaccionComponent } from "./components/transaccion/transaccion.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { LoadingComponent } from "./components/shared/loading/loading.component";
+import { NbThemeModule } from "@nebular/theme";
 
 @NgModule({
   declarations: [
@@ -26,11 +27,8 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     NavbarComponent,
     LoadingComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NbThemeModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
