39fc4880d9030f556d0332ee4c86a4c8d3386b1b
Pablo Piñeiro <pmpv05@hotmail.com>
2019-10-15 16:37:48 -0300

    agrego formato

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { LoginComponent } from "./components/login/login.component";
-import { RegistroComponent } from "./components/registro/registro.component";
-import { IngresoVoluntadComponent } from "./components/ingreso-voluntad/ingreso-voluntad.component";
-import { VoluntadComponent } from "./components/voluntad/voluntad.component";
-import { ListaVoluntadesComponent } from "./components/lista-voluntades/lista-voluntades.component";
-import { PropuestaComponent } from "./components/propuesta/propuesta.component";
-import { TransaccionComponent } from "./components/transaccion/transaccion.component";
-import { NavbarComponent } from "./components/shared/navbar/navbar.component";
-import { LoadingComponent } from "./components/shared/loading/loading.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { IngresoVoluntadComponent } from './components/ingreso-voluntad/ingreso-voluntad.component';
+import { VoluntadComponent } from './components/voluntad/voluntad.component';
+import { ListaVoluntadesComponent } from './components/lista-voluntades/lista-voluntades.component';
+import { PropuestaComponent } from './components/propuesta/propuesta.component';
+import { TransaccionComponent } from './components/transaccion/transaccion.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 import {
   NbThemeModule,
   NbLayoutModule,
   NbSidebarModule,
-  NbButtonModule
-} from "@nebular/theme";
-import { RouterModule } from "@angular/router";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NbEvaIconsModule } from "@nebular/eva-icons";
+  NbButtonModule,
+} from '@nebular/theme';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -33,7 +33,7 @@ import { NbEvaIconsModule } from "@nebular/eva-icons";
     PropuestaComponent,
     TransaccionComponent,
     NavbarComponent,
-    LoadingComponent
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,10 +43,10 @@ import { NbEvaIconsModule } from "@nebular/eva-icons";
     NbSidebarModule.forRoot(),
     NbButtonModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: "default" }),
-    NbEvaIconsModule
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbEvaIconsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
