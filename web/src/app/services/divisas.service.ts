81015242b6be7ca5f4fe4559a6ab7928ab272665
Pablo Piñeiro <pmpv05@hotmail.com>
2019-10-15 14:35:49 -0300

    service divisas

--- web/src/app/services/divisas.service.ts
+++ web/src/app/services/divisas.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { DivisaModel } from "./models/divisa.model";
 import { Observable } from "rxjs";
+import { DivisaCreateModel } from "./models/divisa.create.model";
 
 const API_URL = "http://localhost:8000/divisas";
 
@@ -15,5 +16,25 @@ export class DivisasService {
 
   getDivisas(): Observable<DivisaModel[]> {
     return this.http.get<DivisaModel[]>(API_URL);
+  }
+
+  getDivisa(id: string): Observable<DivisaModel[]> {
+    return this.http.get<DivisaModel[]>(`${API_URL}/${id}`);
+  }
+
+  getDivisaPorCodigo(codigo: string): Observable<DivisaModel[]> {
+    return this.http.get<DivisaModel[]>(`${API_URL}/codigo/${codigo}`);
+  }
+
+  insertDivisa(divisa: DivisaCreateModel): Observable<DivisaModel> {
+    return this.http.post<DivisaModel>(API_URL, divisa);
+  }
+
+  deleteDivisa(id: string): any {
+    return this.http.delete(`${API_URL}/${id}`);
+  }
+
+  updateDivisa(id: string, divisa: DivisaCreateModel): any {
+    return this.http.patch(`${API_URL}/${id}`, divisa);
   }
 }
